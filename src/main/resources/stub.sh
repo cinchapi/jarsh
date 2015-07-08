af850133b3c1bc795917e32bb9c319d8acf9f572
Jeff Nelson <jtnelson1@gmail.com>
2015-07-08 19:06:16 -0400

    doc update and change shebang

--- src/main/resources/stub.sh
+++ src/main/resources/stub.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Adapted from https://coderwall.com/p/ssuaxa
+# This script is embedded in the generated fatjar to give it the ability to call
+# itself when invoked from the command line
 
-# Props to https://coderwall.com/p/ssuaxa
 MYSELF=`which "$0" 2>/dev/null`
 [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
 java=java
@@ -8,4 +10,4 @@ if test -n "$JAVA_HOME"; then
     java="$JAVA_HOME/bin/java"
 fi
 exec "$java" $java_args -jar $MYSELF "$@"
-exit 1 
+exit 1
